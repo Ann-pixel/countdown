64df947e83719143032d8afa11ebdd94c53679f1
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-05-14 11:36:12 -0400

    added styling and basic js.

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // alert("Baaaaaaaaaaaaa! Youre on! 🎇");
-console.log("Hi there!");
+// console.log("Hi there!");
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdown-form");
+const dateEl = document.getElementById("date-picker");
+let countdownTitle = "";
+let countdownDate = "";
+// Set date input min with todays data
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
+
+function updateCountdown(evt) {
+  evt.preventDefault();
+  countdownTitle = evt.srcElement[0].value;
+  countdownDate = evt.srcElement[1].value;
+}
+
+countdownForm.addEventListener("submit", updateCountdown);
